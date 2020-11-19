a9ff4dfc4e4115a89dfe207041ce04b067b26328
Luis Martínez Alonso <luis0pk0rgmail.com>
2020-11-19 09:36:43 +0100

    03 avanzar y fijar

--- PantallaHoraYFecha.java
+++ PantallaHoraYFecha.java
@@ -18,4 +18,42 @@ public class PantallaHoraYFecha
         pantallaMonths = new PantallaDosDigitos (1, 13);
         pantallaYears = new PantallaDosDigitos (1, 100);
     }
+    
+    /**
+     * Devuelve la hora y la fecha
+     */
+    public String getFechaYHora() {
+        return pantallaHoras.getTextoDeLaPantalla() + ":" + pantallaMinutos.getTextoDeLaPantalla() + " " + pantallaDays.getTextoDeLaPantalla() + "-" + pantallaMonths.getTextoDeLaPantalla() + "-" + pantallaYears.getTextoDeLaPantalla();
+    }
+    
+    /**
+     * Avanza de uno en uno los minutos
+     */
+    public void avanzarMinuto() {
+        pantallaMinutos.incrementaValorAlmacenado();
+        if (pantallaMinutos.getValorAlmacenado() == 0) {
+            pantallaHoras.incrementaValorAlmacenado();
+            if (pantallaHoras.getValorAlmacenado() == 0) {
+                pantallaDays.incrementaValorAlmacenado();
+                if (pantallaDays.getValorAlmacenado() == 1) {
+                    pantallaMonths.incrementaValorAlmacenado();
+                    if (pantallaMonths.getValorAlmacenado() == 1) {
+                        pantallaYears.incrementaValorAlmacenado();
+                        
+                    }
+                }
+            }
+        }
+    }
+    
+    /**
+     * Fija los valores de las horas los minutos y la fecha
+     */
+    public void fijarFechaYHora(int horas, int minutos, int days, int months, int years) {
+        pantallaHoras.setValorAlmacenado(horas);
+        pantallaMinutos.setValorAlmacenado(minutos);
+        pantallaDays.setValorAlmacenado(days);
+        pantallaMonths.setValorAlmacenado(months);
+        pantallaYears.setValorAlmacenado(years);
+    }
 }
